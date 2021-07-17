c1ad298a2c9342e399296c8e29b74fe563a757d3
Anthony <anthonyhvelazquez@gmail.com>
2021-07-16 23:01:09 -0400

    Base Models Created

--- portal/admin.py
+++ portal/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Exam, Question, StudentReport
+
+admin.site.register(Exam)
+admin.site.register(Question)
+admin.site.register(StudentReport)
--- portal/models.py
+++ portal/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Question(models.Model):
+  created_at = models.DateTimeField(auto_now_add=True)
+  updated_at = models.DateTimeField(auto_now=True)
+
+  prompt = models.CharField(max_length=255)
+  answer = models.CharField(max_length=255)
+
+class Exam(models.Model):
+  created_at = models.DateTimeField(auto_now_add=True)
+  updated_at = models.DateTimeField(auto_now=True)
+
+  title = models.CharField(max_length=255)
+  questions = models.ManyToManyField(Question, related_name='questions', blank=True, null=True)
+  teacher = models.ForeignKey(User, on_delete=models.CASCADE, related_name='teacher')
+
+class StudentReport(models.Model):
+  score = models.FloatField(default=0)
+  correct_answers = models.IntegerField(default=0)
+  exam = models.ForeignKey(Exam, on_delete=models.CASCADE, related_name='exam')
+  student = models.ForeignKey(User, on_delete=models.CASCADE, related_name='student')
